bd07f905b7efb1629ca88fc6aa6efd9f21cdba3c
MaltSugary <133304682+MaltMa@users.noreply.github.com>
2024-01-13 10:21:13 +0800

    Update washloop.go

--- washloop/washloop.go
+++ washloop/washloop.go
@@ -9,20 +9,20 @@ import (
 	"github.com/shoarai/washout"
 )
 
-// A Motion is accelerations and angular velocities in 3D axis.
+// A Motion is accelerations and angular velocities in 3D axis.运动是3D轴上的加速度和角速度。
 type Motion struct {
 	Acceleration    washout.Vector
 	AngularVelocity washout.Vector
 }
 
-// A WashoutInterface is a interface of washout.
+// A WashoutInterface is a interface of washout.WashoutInterface是一个washout接口。
 type WashoutInterface interface {
 	Filter(
 		accelerationX, accelerationY, accelerationZ,
 		angularVelocityX, angularVelocityY, angularVelocityZ float64) washout.Position
 }
 
-// A WashoutLoop is a loop for process of washout.
+// A WashoutLoop is a loop for process of washout.WashoutLoop是用于清洗过程的循环。
 type WashoutLoop struct {
 	interval uint
 
@@ -36,7 +36,7 @@ type WashoutLoop struct {
 	positionMutex *sync.Mutex
 }
 
-// NewWashLoop creates new washout loop.
+// NewWashLoop creates new washout loop.NewWashLoop创建新的洗出循环。
 func NewWashLoop(washout WashoutInterface, interval uint) *WashoutLoop {
 	w := WashoutLoop{}
 	w.stopCh = make(chan struct{})
@@ -45,7 +45,7 @@ func NewWashLoop(washout WashoutInterface, interval uint) *WashoutLoop {
 	return &w
 }
 
-// Start starts a loop of process.
+// Start starts a loop of process.Start启动进程的循环。
 func (w *WashoutLoop) Start() {
 	interval := time.Duration(w.interval) * time.Millisecond
 	ticker := time.NewTicker(interval)
@@ -61,7 +61,7 @@ func (w *WashoutLoop) Start() {
 	}
 }
 
-// Stop stops a loop of process.
+// Stop stops a loop of process.Stop停止进程的循环。
 func (w *WashoutLoop) Stop() {
 	close(w.stopCh)
 }
@@ -85,7 +85,7 @@ func (w *WashoutLoop) init(washout WashoutInterface) {
 	w.positionMutex = new(sync.Mutex)
 }
 
-// SetMotion sets a motion used as input of washout.
+// SetMotion sets a motion used as input of washout.SetMotion设置用作冲洗输入的运动。
 func (w *WashoutLoop) SetMotion(motion Motion) {
 	w.motionMutex.Lock()
 	defer w.motionMutex.Unlock()
@@ -107,7 +107,7 @@ func (w *WashoutLoop) setPosition(position washout.Position) {
 	w.position = position
 }
 
-// GetPosition gets a position as output of washout.
+// GetPosition gets a position as output of washout.GetPosition获取一个位置作为washout的输出。
 func (w *WashoutLoop) GetPosition() washout.Position {
 	w.positionMutex.Lock()
 	defer w.positionMutex.Unlock()
